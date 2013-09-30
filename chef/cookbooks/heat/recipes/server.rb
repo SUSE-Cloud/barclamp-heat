ff770d4f4bc0e1a63d136785298ce5902801d80a
slevchenko <slevchenko@slevchenko-pc.(none)>
2013-09-30 14:25:02 +0300

    Following changes were made:     1. Fixed pfs deployment scenario.     2. Minor idiomatic changes.

--- chef/cookbooks/heat/recipes/server.rb
+++ chef/cookbooks/heat/recipes/server.rb
@@ -15,16 +15,52 @@
 
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
-node.set_unless[:heat][:db][:password] = secure_password
+heat_conf = {}
+heat_conf["ubuntu"] = { :packages => ["heat-engine","heat-api","heat-api-cfn","heat-api-cloudwatch"],
+                       :services => ["heat-engine","heat-api","heat-api-cfn","heat-api-cloudwatch"],
+                       :aux_dirs => ["/var/cache/heat"]
+}
+heat_conf["suse"] = { :packages => ["openstack-heat-engine","openstack-heat-api","openstack-heat-api-cfn","openstack-heat-api-cloudwatch","python-heatclient"],
+                     :services => ["openstack-heat-engine","openstack-heat-api","openstack-heat-api-cfn","openstack-heat-api-cloudwatch"],
+                     :aux_dirs => ["/var/cache/heat"]
+}
 
+#case node.platform
+#    when "ubuntu"
+#        puts "Installing heat on Ubuntu..."
+#        used_packages = heat_conf[:ubuntu_packages]
+#        used_services = heat_conf[:ubuntu_services]
+#        aux_dirs = heat_conf[:ubuntu_aux_dirs]
+#    when "suse"
+#        puts "Installing heat Suse"
+#        used_packages = heat_conf[:suse_packages]
+#        used_services = heat_conf[:suse_services]
+#        aux_dirs = heat_conf[:suse_aux_dirs]
+#    else
+#        raise RuntimeError, 'ERROR: Failed to determine OS aborting...'
+#end
+
+
+heat_path = "/opt/heat"
+venv_path = node[:heat][:use_virtualenv] ? "#{heat_path}/.venv" : nil
+venv_prefix = node[:heat][:use_virtualenv] ? ". #{venv_path}/bin/activate &&" : nil
+
+
+
+
+node.set_unless[:heat][:db][:password] = secure_password
 env_filter = " AND database_config_environment:database-config-#{node[:heat][:database_instance]}"
-sqls = search(:node, "roles:database-server#{env_filter}") || []
-if sqls.length > 0
-    sql = sqls[0]
-    sql = node if sql.name == node.name
-else
-    sql = node
-end
+sql = search(:node, "roles:database-server#{env_filter}").first || node
+
+
+#sqls = search(:node, "roles:database-server#{env_filter}") || []
+#if sqls.length > 0
+#    sql = sqls[0]
+#    sql = node if sql.name == node.name
+#else
+#    sql = node
+#end
+
 include_recipe "database::client"
 backend_name = Chef::Recipe::Database::Util.get_backend_name(sql)
 include_recipe "#{backend_name}::client"
@@ -70,47 +106,79 @@ database_user "grant database access for heat database user" do
 end
 
 unless node[:heat][:use_gitrepo]
-  unless node.platform == "suse"
-    package "heat-api"
-    package "python-heatclient"
-  else
-    package "openstack-heat-api"
-    package "openstack-heat-api-cfn"
-    package "openstack-heat-api-cloudwatch"
-    package "openstack-heat-engine"
-    package "python-heatclient"
-  end
-  venv_prefix = nil
+    heat_conf[node.platform][:packages].each do |p|
+        puts "Processing cookbook: #{@cookbook_name} package: #{p} platform: #{node.platform}"
+        package p
+    end
+
+#  unless node.platform == "suse"
+#    package "heat-api"
+#    package "python-heatclient"
+#  else
+#    package "openstack-heat-api"
+#    package "openstack-heat-api-cfn"
+#    package "openstack-heat-api-cloudwatch"
+#    package "openstack-heat-engine"
+#    package "python-heatclient"
+#  end
+#  venv_prefix = nil
 else
-  heat_path = "/opt/heat"
-  pfs_and_install_deps("heat")
-  link_service "heat-api"
-  create_user_and_dirs("heat")
-  execute "cp_policy.json" do
-    command "cp #{heat_path}/etc/policy.json /etc/heat"
-    creates "/etc/heat/policy.json"
-  end
-  venv_path = node[:heat][:use_virtualenv] ? "#{heat_path}/.venv" : nil
-  venv_prefix = node[:heat][:use_virtualenv] ? ". #{venv_path}/bin/activate &&" : nil
+    puts "Instaling from sources, cookbook: #{@cookbook_name} platform: #{node.platform}"
+    pfs_and_install_deps @cookbook_name do
+        virtualenv venv_path
+        path heat_path
+    end
+    
+    heat_conf[node.platform][:services].each do |s|
+        puts "Linking #{s}..."
+        link_service s
+    end 
+   
+    heat_conf[node.platform][:aux_dirs].each do |d|
+        puts "Creating auxilary directory #{d}..." 
+        directory d do
+           owner node[:heat][:user]
+           group "root"
+           mode 00755
+           action :create 
+        end
+    end
 end
+#  heat_path = "/opt/heat"
+#  pfs_and_install_deps("heat")
+#  link_service "heat-api"
+#  link_service "heat-engine"
+#  link_service "heat-api-cfn"
+#  link_service "heat-api-cloudwatch"
+#  create_user_and_dirs("heat")
+#  execute "cp_policy.json" do
+#    command "cp #{heat_path}/etc/heat/policy.json /etc/heat"
+#    creates "/etc/heat/policy.json"
+#  end
+#  venv_path = node[:heat][:use_virtualenv] ? "#{heat_path}/.venv" : nil
+#  venv_prefix = node[:heat][:use_virtualenv] ? ". #{venv_path}/bin/activate &&" : nil
+#end
 
 include_recipe "#{@cookbook_name}::common"
 
-directory "/var/cache/heat" do
-  owner node[:heat][:user]
-  group "root"
-  mode 00755
-  action :create
-end unless node.platform == "suse"
+#directory "/var/cache/heat" do
+#  owner node[:heat][:user]
+#  group "root"
+#  mode 00755
+#  action :create
+#end unless node.platform == "suse"
 
 env_filter = " AND rabbitmq_config_environment:rabbitmq-config-#{node[:heat][:rabbitmq_instance]}"
-rabbits = search(:node, "roles:rabbitmq-server#{env_filter}") || []
-if rabbits.length > 0
-  rabbit = rabbits[0]
-  rabbit = node if rabbit.name == node.name
-else
-  rabbit = node
-end
+rabbit = search(:node, "roles:rabbitmq-server#{env_filter}").first || node
+
+#rabbits = search(:node, "roles:rabbitmq-server#{env_filter}") || []
+#if rabbits.length > 0
+#  rabbit = rabbits[0]
+#  rabbit = node if rabbit.name == node.name
+#else
+#  rabbit = node
+#end
+
 rabbit_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(rabbit, "admin").address
 Chef::Log.info("Rabbit server found at #{rabbit_address}")
 rabbit_settings = {
@@ -122,13 +190,15 @@ rabbit_settings = {
 }
 
 env_filter = " AND keystone_config_environment:keystone-config-#{node[:heat][:keystone_instance]}"
-keystones = search(:node, "recipes:keystone\\:\\:server#{env_filter}") || []
-if keystones.length > 0
-  keystone = keystones[0]
-  keystone = node if keystone.name == node.name
-else
-  keystone = node
-end
+keystone = search(:node, "recipes:keystone\\:\\:server#{env_filter}").first || node
+
+#keystones = search(:node, "recipes:keystone\\:\\:server#{env_filter}") || []
+#if keystones.length > 0
+#  keystone = keystones[0]
+#  keystone = node if keystone.name == node.name
+#else
+#  keystone = node
+#end
 
 keystone_host = keystone[:fqdn]
 keystone_protocol = keystone["keystone"]["api"]["protocol"]
@@ -160,10 +230,11 @@ if node.roles.include? "heat-server"
   db_password = node[:heat][:db][:password]
 else
   # pickup password to database from heat-server node
-  node_controllers = search(:node, "roles:heat-server") || []
-  if node_controllers.length > 0
-    db_password = node_controllers[0][:heat][:db][:password]
-  end
+  node_controllers = search(:node, "roles:heat-server").first || []
+#  node_controllers = search(:node, "roles:heat-server") || []
+#  if node_controllers.length > 0
+#    db_password = node_controllers[0][:heat][:db][:password]
+#  end
 end
 
 
@@ -246,6 +317,7 @@ keystone_register "register heat endpoint" do
   action :add_endpoint_template
 end
 
+
 template "/etc/heat/heat-api.conf" do
     source "heat-api.conf.erb"
     owner node[:heat][:user]
@@ -315,6 +387,15 @@ template "/etc/heat/heat-api-cfn.conf" do
       :cfn_port => node[:heat][:api][:cfn_port]
     )
 end
+
+service "heat-api" do
+  service_name "openstack-heat-api" if node.platform == "suse"
+  supports :status => true, :restart => true
+  action :enable
+  subscribes :restart, resources("template[/etc/heat/heat-api.conf]")
+end
+
+
 
 template "/etc/heat/heat-api-cfn-paste.ini" do
     source "heat-api-cfn-paste.ini.erb"
