aada02a407e52ed6628a19395fbec2a83da90b36
SergeyLevchenko <slevchenko@mirantis.com>
2013-10-01 14:51:12 +0300

    Minor monitor.rb improvements

--- chef/cookbooks/heat/recipes/monitor.rb
+++ chef/cookbooks/heat/recipes/monitor.rb
@@ -1,21 +1,24 @@
 svcs = []
-node[:heat][:api].each_key do |svc|
-   svcs << svc
+ports = {}
+node.default[:heat][:platform][:services].each {|svc| svcs << svc}
+node.default[:heat][:api].each do |k,v|
+    next if k !~ /port*/
+    ports[k] = v
 end
-log ("Will monitor heat services: ")
+
+log ("Will monitor heat services: #{svcs.inspect}")
 
 include_recipe "nagios::common" if node["roles"].include?("nagios-client")
 
-template "/etc/nagios/nrpe.d/heat-api_nrpe.cfg" do
+template "/etc/nagios/nrpe.d/heat_nrpe.cfg" do
   source "heat_nrpe.cfg.erb"
   mode "0644"
   group node[:nagios][:group]
   owner node[:nagios][:user]
   variables( {
     :heat_services => svcs,
-    :heat_ports => node[:heat][:api],
+    :heat_ports => ports,
     :heat_ip => node.ipaddress
-  }
+  })
    notifies :restart, "service[nagios-nrpe-server]"
 end if node["roles"].include?("nagios-client")
-
