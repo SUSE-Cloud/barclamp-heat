55173c89227736f69ce27026e2f723bcf96eefc3
Vincent Untz <vuntz@suse.com>
2015-09-09 11:17:54 +0200

    Add note that this got moved to merged repo

--- chef/data_bags/crowbar/migrate/heat/012_add_instance_user.rb
+++
@@ -1,9 +0,0 @@
-def upgrade ta, td, a, d
-  a['default_instance_user'] = ta['default_instance_user']
-  return a, d
-end
-
-def downgrade ta, td, a, d
-  a.delete('default_instance_user')
-  return a, d
-end
--- chef/roles/heat-server.rb
+++
@@ -1,9 +0,0 @@
-name "heat-server"
-description "Heat Server Role"
-run_list(
-         "recipe[heat::server]",
-         "recipe[heat::monitor]"
-)
-default_attributes()
-override_attributes()
-
